61c64fc65b831936430ed9464b3b1ed37580c34f
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-06-28 17:31:03 +0200

    MenubarCreator completed

--- UltSigPro/src/main/java/gui/menubar/FileMenuItem.java
+++ UltSigPro/src/main/java/gui/menubar/FileMenuItem.java
@@ -4,12 +4,12 @@ import i18n.LanguageResourceHandler;
 import javafx.scene.control.MenuItem;
 import resourceframework.ResourceProviderException;
 
-public class HelpMenuItem extends MenuItem {
-	
+public class FileMenuItem extends MenuItem {
+
 	private static final String TITLE = "title";
 	
-	public HelpMenuItem() throws ResourceProviderException {
-		super(LanguageResourceHandler.getInstance().getLocalizedText(HelpMenuItem.class, TITLE));
+	public FileMenuItem() throws ResourceProviderException {		
+		super(LanguageResourceHandler.getInstance().getLocalizedText(FileMenuItem.class, TITLE));	
 	}
 	
 }
--- UltSigPro/src/main/java/gui/menubar/MenuBarCreator.java
+++ UltSigPro/src/main/java/gui/menubar/MenuBarCreator.java
@@ -7,6 +7,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -16,6 +17,9 @@ import org.xml.sax.SAXException;
 import i18n.LanguageResourceHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import logging.CommonLogger;
 import resourceframework.GlobalResourceProvider;
 import resourceframework.ResourceProviderException;
 
@@ -40,27 +44,61 @@ public class MenuBarCreator {
 			if(menuBarNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element menuBarElement = (Element) menuBarNode;
 				
-				NodeList menuNodeList = menuBarElement.getElementsByTagName("menu");
+				NodeList menuNodeList = menuBarElement.getChildNodes();
 				
 				for(int j=0; j<menuNodeList.getLength(); j++) {
-					Element menuElement = (Element) menuNodeList.item(j);
-					// Get menu title
-					NodeList titleNodeList = menuElement.getElementsByTagName("title");
-					Element titleElement = (Element) titleNodeList.item(0);
-					menuBar.getMenus().add(new Menu(LanguageResourceHandler.getInstance().getLocalizedText(titleElement.getTextContent())));
+					Node menuNode = menuNodeList.item(j);
 					
-					// Get list of class entrys
-					NodeList classNodeList = menuElement.getElementsByTagName("class");
-					
-					for(int a=0; a<classNodeList.getLength(); a++) {
-						Element classElement = (Element) classNodeList.item(a);
-						System.out.println(classElement.getTextContent());
+					if(menuNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element menuElement = (Element) menuNode;
+						
+						Menu curMenu;
+						try {
+							curMenu = getMenu(menuElement);
+							menuBar.getMenus().add(curMenu);
+						} catch (DOMException | ClassNotFoundException | InstantiationException
+								| IllegalAccessException e) {
+							CommonLogger.getInstance().logException(e);
+						}						
 					}
 				}
 			}
 		}
 		
 		return menuBar;
+	}
+	
+	private Menu getMenu(Element menuElement) throws DOMException, ResourceProviderException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+		Menu curMenu = new Menu();
+		
+		NodeList childNodes = menuElement.getChildNodes();
+		
+		for(int i=0; i<childNodes.getLength(); i++) {
+			Node curNode = childNodes.item(i);
+			if(curNode.getNodeType()  == Node.ELEMENT_NODE) {
+				Element curElement = (Element) curNode;
+				
+				if(curElement.getTagName().equals("title")) {
+					String title = LanguageResourceHandler.getInstance().getLocalizedText(curElement.getTextContent());
+					curMenu.setText(title);
+				} else if(curElement.getTagName().equals("class")) {
+					String className = curElement.getTextContent();
+					if(className != null && !className.isEmpty()) {
+						Class<?> itemClass = Class.forName(className);
+						MenuItem curItem = (MenuItem) itemClass.newInstance();
+						curMenu.getItems().add(curItem);								
+					}
+				} else if (curElement.getTagName().equals("separator")) {
+					curMenu.getItems().add(new SeparatorMenuItem());
+				} else {
+					Menu childMenu = getMenu(curElement);
+					curMenu.getItems().add(childMenu);
+				}				
+			}
+		}
+		
+		return curMenu;
 	}
 	
 }
