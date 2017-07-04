5da9227b474c46de840c1e8d8f0c206be64b4d01
RolandMueller1994 <roland.mueller.1994@freenet.de>
2017-07-04 23:15:18 +0200

    Changes in TestKlasse

--- UltSigPro/src/main/java/inputHandler/TestKlasse.java
+++ UltSigPro/src/main/java/inputHandler/TestKlasse.java
@@ -8,12 +8,12 @@ public class TestKlasse {
 		InputAdministrator.getInputAdminstrator();
 		InputAdministrator.getInputAdminstrator().collectSoundInputDevices();
 		System.out.println("All devices: " + InputAdministrator.getInputAdminstrator().getInputDevices());
-		Channel channel1 = new Channel();
-		channel1.setSubscription("Prim�rer Soundaufnahmetreiber");
+		//Channel channel1 = new Channel();
+		//channel1.setSubscription("Prim�rer Soundaufnahmetreiber");
 		System.out.println("Subscribed Devices: " + InputAdministrator.getInputAdminstrator().getSubscribedDevicesName());
 		System.out.println("Open Lines: " + InputAdministrator.getInputAdminstrator().getTargetDataLines());
 		InputAdministrator.getInputAdminstrator().startListening();
-		channel1.removeSubscription("Prim�rer Soundaufnahmetreiber");
+		//channel1.removeSubscription("Prim�rer Soundaufnahmetreiber");
 		System.out.println("Subscription removed!");
 		System.out.println("Subscribed Devices: " + InputAdministrator.getInputAdminstrator().getSubscribedDevicesName());
 		System.out.println("Open Lines: " + InputAdministrator.getInputAdminstrator().getTargetDataLines());
