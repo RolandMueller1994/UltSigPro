0fcfeda87b80cd4b41f3807e8d55b337831ed7ea
Kone <Kone@Kone-Notebook.fritz.box>
2017-10-20 20:34:17 +0200

    java doc

--- UltSigPro/src/main/java/gui/menubar/SaveMenuItem.java
+++ UltSigPro/src/main/java/gui/menubar/SaveMenuItem.java
@@ -8,6 +8,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -28,6 +29,15 @@ import javafx.stage.FileChooser;
 import resourceframework.GlobalResourceProvider;
 import resourceframework.ResourceProviderException;
 
+/**
+ * Saves the project settings (channel name, input devices, output devices, ...)
+ * in an .usp file. If called for the first time, a path for the file needs to
+ * be specified. Afterwards, the file gets automatically overwritten when this
+ * {@linkplain MenuItem} is selected.
+ * 
+ * @author Kone
+ *
+ */
 public class SaveMenuItem extends MenuItem {
 
 	private static final String TITLE = "title";
@@ -45,7 +55,8 @@ public class SaveMenuItem extends MenuItem {
 					GlobalResourceProvider prov = GlobalResourceProvider.getInstance();
 					if (prov.checkRegistered("projectFile")) {
 						File file = (File) prov.getResource("projectFile");
-						collectProjectSettings(file);
+						Document doc = collectProjectSettings();
+						createXMLFile(doc, file);
 
 					} else {
 						FileChooser fileChooser = new FileChooser();
@@ -54,14 +65,22 @@ public class SaveMenuItem extends MenuItem {
 						File file = fileChooser.showSaveDialog(USPGui.stage);
 
 						if (file != null) {
-							// TODO create USP file with project settings
 							prov.registerResource("projectFile", file);
-							collectProjectSettings(file);
-
+							Document doc = collectProjectSettings();
+							createXMLFile(doc, file);
 						}
 					}
 
 				} catch (ResourceProviderException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TransformerConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (TransformerException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
@@ -69,65 +88,74 @@ public class SaveMenuItem extends MenuItem {
 		});
 	}
 
-	private void collectProjectSettings(File file) {
-		try {
+	/**
+	 * Creates an XML file at the specified path with the given text and
+	 * structure.
+	 * 
+	 * @param doc
+	 *            Contains the structure and elements which have to be written
+	 *            into the file.
+	 * @param file
+	 *            Is the file with the file name and path.
+	 * @throws TransformerConfigurationException
+	 * @throws TransformerException
+	 */
+	private void createXMLFile(Document doc, File file) throws TransformerConfigurationException, TransformerException {
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer;
+		transformer = transformerFactory.newTransformer();
+		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(file);
+		transformer.transform(source, result);
+	}
 
-			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-			Document doc = docBuilder.newDocument();
+	/**
+	 * Collects the informations of every {@linkplain Channel} and builds the
+	 * structure for an XML file.
+	 * 
+	 * @return The structure with the values for the XML file.
+	 * @throws ParserConfigurationException
+	 */
+	private Document collectProjectSettings() throws ParserConfigurationException {
 
-			// root elements
-			Element rootElement = doc.createElement("UspProjectFile");
-			doc.appendChild(rootElement);
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.newDocument();
 
-			Iterator<Node> iterChannelName = USPGui.getChannelBox().getChildren().iterator();
-			while (iterChannelName.hasNext()) {
-				ChannelPane curElement = (ChannelPane) iterChannelName.next();
-				// channel elements
-				Element channelName = doc.createElement(curElement.getName());
-				rootElement.appendChild(channelName);
+		// root elements
+		Element rootElement = doc.createElement("UspProjectFile");
+		doc.appendChild(rootElement);
 
-				// set attribute to channelName element
-				// channelName.setAttribute("id", "1");
+		Iterator<Node> iterChannelName = USPGui.getChannelBox().getChildren().iterator();
+		while (iterChannelName.hasNext()) {
+			ChannelPane curElement = (ChannelPane) iterChannelName.next();
+			// channel elements
+			Element channelName = doc.createElement(curElement.getName());
+			rootElement.appendChild(channelName);
 
-				// input device elements
-				ObservableList<String> inputDevices = curElement.getInputPaneTableItems();
-				for (String inputDevice : inputDevices) {
-					Element inputDeviceName = doc.createElement("inputDevice");
-					inputDeviceName.appendChild(doc.createTextNode(inputDevice));
-					channelName.appendChild(inputDeviceName);
-				}
+			// set attribute to channelName element
+			// channelName.setAttribute("id", "1");
 
-				// output device element
-				ObservableList<String> outputDevices = curElement.getOutputPaneTableItems();
-				for (String outputDevice : outputDevices) {
-					Element outputDeviceName = doc.createElement("outputDevice");
-					outputDeviceName.appendChild(doc.createTextNode(outputDevice));
-					channelName.appendChild(outputDeviceName);
-				}
+			// input device elements
+			ObservableList<String> inputDevices = curElement.getInputPaneTableItems();
+			for (String inputDevice : inputDevices) {
+				Element inputDeviceName = doc.createElement("inputDevice");
+				inputDeviceName.appendChild(doc.createTextNode(inputDevice));
+				channelName.appendChild(inputDeviceName);
 			}
 
-			// write the content into xml file
-			TransformerFactory transformerFactory = TransformerFactory.newInstance();
-			Transformer transformer = transformerFactory.newTransformer();
-			transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "3");
-			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(file);
-
-			// Output to console for testing
-			// StreamResult result = new StreamResult(System.out);
-
-			transformer.transform(source, result);
-
-			System.out.println("File saved!");
-
-		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace();
-		} catch (TransformerException tfe) {
-			tfe.printStackTrace();
+			// output device element
+			ObservableList<String> outputDevices = curElement.getOutputPaneTableItems();
+			for (String outputDevice : outputDevices) {
+				Element outputDeviceName = doc.createElement("outputDevice");
+				outputDeviceName.appendChild(doc.createTextNode(outputDevice));
+				channelName.appendChild(outputDeviceName);
+			}
 		}
+		return doc;
 	}
 
 }
