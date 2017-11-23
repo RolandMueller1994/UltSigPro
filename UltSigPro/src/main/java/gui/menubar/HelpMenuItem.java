ad44c649558d6fba281129b7bd946abe331d5b6a
Kone <Kone@Kone-Notebook.fritz.box>
2017-11-23 20:13:59 +0100

    commit for merge

--- UltSigPro/src/main/java/gui/menubar/HelpMenuItem.java
+++ UltSigPro/src/main/java/gui/menubar/HelpMenuItem.java
@@ -1,15 +1,21 @@
 package gui.menubar;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Locale;
+
 import i18n.LanguageResourceHandler;
 import javafx.scene.control.MenuItem;
+import resourceframework.GlobalResourceProvider;
 import resourceframework.ResourceProviderException;
 
 public class HelpMenuItem extends MenuItem {
 	
 	private static final String TITLE = "title";
 	
-	public HelpMenuItem() throws ResourceProviderException {
+	public HelpMenuItem() throws ResourceProviderException, IOException {
 		super(LanguageResourceHandler.getInstance().getLocalizedText(HelpMenuItem.class, TITLE));
+		
 	}
-	
 }
