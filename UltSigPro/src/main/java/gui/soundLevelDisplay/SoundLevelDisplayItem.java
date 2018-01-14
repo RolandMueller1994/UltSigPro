2daf43cdced9f4a064c5dbc358696de0b5892a73
Kone <Kone@Kone-Notebook>
2018-01-14 15:09:48 +0100

    implemented AnimationTimer in SoundLevelDisplayItems

--- UltSigPro/src/main/java/gui/soundLevelDisplay/SoundLevelDisplayItem.java
+++ UltSigPro/src/main/java/gui/soundLevelDisplay/SoundLevelDisplayItem.java
@@ -1,10 +1,9 @@
 package gui.soundLevelDisplay;
 
 import java.util.LinkedList;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import javafx.scene.control.ProgressBar;
+import javafx.animation.AnimationTimer;
 import javafx.geometry.HPos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -13,30 +12,45 @@ public class SoundLevelDisplayItem extends GridPane {
 
 	private Label deviceNameField;
 	private ProgressBar soundLevelBar;
-	private ProgressBar overdriveIndicator;
-	private LinkedList<LinkedList<Integer>> dataQueue;
+	private final int itemWidth = 120; 
 
 	private LinkedList<Integer> internalBuffer = new LinkedList<>();
-	private ScheduledThreadPoolExecutor executor;
+	
+	private AnimationTimer animationTimer;
 
 	public SoundLevelDisplayItem(String deviceName, LinkedList<LinkedList<Integer>> dataQueue) {
 
 		deviceNameField = new Label(deviceName);
-		deviceNameField.setPrefWidth(120);
-		deviceNameField.setMaxWidth(120);
+		deviceNameField.setPrefWidth(itemWidth);
+		deviceNameField.setMaxWidth(itemWidth);
+		
 		soundLevelBar = new ProgressBar(0.1);
 		soundLevelBar.setStyle("-fx-accent: -usp-dark-grey");
-		overdriveIndicator = new ProgressBar(0);
-		overdriveIndicator.setStyle("-fx-accent: red");
-		overdriveIndicator.setMaxWidth(10);
+		soundLevelBar.setPrefWidth(itemWidth);
+		soundLevelBar.setMaxWidth(itemWidth);
+		
 		this.setVgap(5);
 		this.setHgap(3);
-		this.add(deviceNameField, 0, 0, 2, 1);
+		this.add(deviceNameField, 0, 0);
 		this.add(soundLevelBar, 0, 1);
-		this.add(overdriveIndicator, 1, 1);
-		GridPane.setHalignment(soundLevelBar, HPos.CENTER);
 
-		this.dataQueue = dataQueue;
+		
+		animationTimer = new AnimationTimer() {
+
+			@Override
+			public void handle(long now) {
+				LinkedList<LinkedList<Integer>> data = new LinkedList<>();
+
+				synchronized (dataQueue) {
+					data.addAll(dataQueue);
+					dataQueue.clear();
+				}
+
+				for (LinkedList<Integer> list : data) {
+					setSoundLevel(list);
+				}
+			}
+		};
 	}
 
 	private void setSoundLevel(LinkedList<Integer> soundValues) {
@@ -59,11 +73,12 @@ public class SoundLevelDisplayItem extends GridPane {
 			if (maxValue < -30) {
 				maxValue = -30;
 			}
-			this.soundLevelBar.setProgress((30 + maxValue) / 30);
+			soundLevelBar.setProgress((30 + maxValue) / 30);
+			
 			if (maxValue > -3) {
-				overdriveIndicator.setProgress(100);
+				soundLevelBar.setStyle("-fx-accent: red");
 			} else {
-				overdriveIndicator.setProgress(0);
+				soundLevelBar.setStyle("-fx-accent: -usp-dark-grey");
 			}
 			internalBuffer.clear();
 		}
@@ -74,32 +89,9 @@ public class SoundLevelDisplayItem extends GridPane {
 		internalBuffer.clear();
 
 		if (play) {
-
-			Runnable evaluationRunnable = new Runnable() {
-
-				@Override
-				public void run() {
-					LinkedList<LinkedList<Integer>> data = new LinkedList<>();
-
-					synchronized (dataQueue) {
-						data.addAll(dataQueue);
-						dataQueue.clear();
-					}
-
-					for (LinkedList<Integer> list : data) {
-						setSoundLevel(list);
-					}
-				}
-
-			};
-
-			executor = new ScheduledThreadPoolExecutor(1);
-
-			executor.scheduleAtFixedRate(evaluationRunnable, 0, 25, TimeUnit.MILLISECONDS);
+			animationTimer.start();
 		} else {
-			executor.shutdownNow();
+			animationTimer.stop();
 		}
-
 	}
-
 }
